942e98bcb6fbdf4c98accfd06522d2edfe038efa
kujevla <150625061+kujevla@users.noreply.github.com>
2024-03-07 11:03:57 +0200

    Update E1A3.py

--- E1A3.py
+++ E1A3.py
@@ -1,1 +1,15 @@
+user =input("PLITHOS ORWN MONO AKERWN THETIKWN:===>")
+while True:
+ user =input("PLITHOS ORWN MONO AKERWN THETIKWN:===>")
+ if user.isdigit():
+  user=int(user)
+  break
+ else:
+   user =input("mono akereoi thetikoi")
 
+#apo=0#apotelesma teliko
+count=1
+for x in range(1,user,1):
+ count=count*2
+ d=1+1/x*2
+print(d)
